bba4dabea4e26f45354dbc5f2a04535c5145513d
Kent Dang <kenttdang@gmail.com>
2024-02-06 19:54:38 -0500

    Added Pomdoro styling

--- study-flow/src/app/app-routing.module.ts
+++ study-flow/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PomodoroComponent } from './pomodoro/pomodoro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pomodoro', component: PomodoroComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- study-flow/src/app/app.module.ts
+++ study-flow/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PomodoroComponent } from './pomodoro/pomodoro.component';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { PomodoroComponent } from './pomodoro/pomodoro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
