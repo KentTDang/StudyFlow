1067df30ce722185951c3ae8b4db81cd7f6f9d92
CivicPie <kent_2015@outlook.com>
2024-03-03 22:59:33 -0500

    Remove Side Navigation Bar and To-Do component. Remove the routing to these pages. Remove margin left on submit button.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PomodoroComponent } from './pages/pomodoro/pomodoro.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
 
 const routes: Routes = [
-  { path: '', component: PomodoroComponent},
-  // { path: '', loadChildren: () => import('./side-nav/side-nav.router.module').then(x => x.SideNavRouterModule)},
-  { path: 'study-flow', loadChildren: () => import('./side-nav/side-nav.router.module').then(x => x.SideNavRouterModule)}
-
+  { path: '', component: PomodoroComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PomodoroComponent } from './pages/pomodoro/pomodoro.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import { ToDoComponent } from './pages/to-do/to-do.component';
 import { MatListModule } from '@angular/material/list';
 import {MatDialogModule} from '@angular/material/dialog'; 
 import { InfoDialogComponent } from './dialogs/info-dialog/info-dialog.component';
@@ -21,8 +19,6 @@ import {MatIconModule} from '@angular/material/icon';
   declarations: [
     AppComponent,
     PomodoroComponent,
-    SideNavComponent,
-    ToDoComponent,
     InfoDialogComponent,
     FeedbackDialogComponent,
     AboutDialogComponent
